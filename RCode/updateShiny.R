2b53f8056dad9ffc1dff421ca88006cbf835e6ee
Christoph Schwerdtfeger <christoph.schwerdtfeger@gmail.com>
2023-08-28 14:23:23 +0200

    Move setAccountInfo into updateShiny.R

--- RCode/updateShiny.R
+++ RCode/updateShiny.R
@@ -5,6 +5,12 @@ updateShiny <- function (Ergebnis, Ergebnis2, Ergebnis3,
   
   library(rsconnect)
   
+  account_name <- Sys.getenv("SHINYAPPS_IO_ACCOUNT")
+  account_token <- Sys.getenv("SHINYAPPS_IO_TOKEN")
+  account_secret <- Sys.getenv("SHINYAPPS_IO_SECRET")
+
+  rsconnect::setAccountInfo(name = account_name, token = account_token, secret = account_secret)
+
   curr_wd <- getwd()
   setwd(directory)
   save(Ergebnis, Ergebnis2, Ergebnis3, Ergebnis3_Aufstieg, file = "data/Ergebnis.Rds")
