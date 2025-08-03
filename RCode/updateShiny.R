9b3e39b6a0305413f28e4cb1410cd62000d1569f
chrisschwer <christoph.schwerdtfeger@gmail.com>
2025-08-03 20:18:52 +0200

    refactor: adapt updateShiny.R and updateScheduler.R for local development
    - Update updateShiny.R with local directory path for ShinyApp
    - Fix string concatenation in updateScheduler.R using paste0()
    - Both files now work correctly on local machine

--- RCode/updateShiny.R
+++ RCode/updateShiny.R
@@ -1,6 +1,6 @@
 updateShiny <- function (Ergebnis, Ergebnis2, Ergebnis3,  
                          Ergebnis3_Aufstieg = Ergebnis3,
-                         directory = "/ShinyApp",
+                         directory = "/Users/christophschwerdtfeger/Library/CloudStorage/Dropbox-CSDataScience/Christoph Schwerdtfeger/Coding Projects/LeagueSimulator_Claude/League-Simulator-Update/ShinyApp",
                          forceUpdate = TRUE) {
   
   library(rsconnect)
